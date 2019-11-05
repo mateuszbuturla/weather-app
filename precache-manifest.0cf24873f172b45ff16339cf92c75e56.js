cc60a62f90a358fd861a623c7d1377e633f7fd15
Mateusz Buturla <2002mateuszbuturla@gmail.com>
2019-11-05 20:22:05 +0100

    Updates

---
+++ precache-manifest.0cf24873f172b45ff16339cf92c75e56.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "a53b7ba4fd4db778f6b4fa55f85e99fc",
+    "url": "/weather-app/static/media/bg_sunset.a53b7ba4.jpg"
+  },
+  {
+    "revision": "8233f615688994355e38abc79fb09bab",
+    "url": "/weather-app/static/media/bg_sunrise.8233f615.jpg"
+  },
+  {
+    "revision": "264eb301f8dccc71c588604be6f2a8f9",
+    "url": "/weather-app/static/media/bg_night.264eb301.png"
+  },
+  {
+    "revision": "a1bec56a9cab24ff6ac803256df3e69f",
+    "url": "/weather-app/static/media/bg_day.a1bec56a.jpg"
+  },
+  {
+    "revision": "90e6a6c358468ae00209",
+    "url": "/weather-app/static/js/runtime~main.4606d7f5.js"
+  },
+  {
+    "revision": "9af5d5e14855570692da",
+    "url": "/weather-app/static/js/main.4f5e8228.chunk.js"
+  },
+  {
+    "revision": "2095128ee6166299d4f9",
+    "url": "/weather-app/static/js/2.b366260f.chunk.js"
+  },
+  {
+    "revision": "9af5d5e14855570692da",
+    "url": "/weather-app/static/css/main.2fc52373.chunk.css"
+  },
+  {
+    "revision": "dec1e7e657889fe8c6a39934278245fb",
+    "url": "/weather-app/index.html"
+  }
+];
